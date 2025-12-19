bbed7d9db657467ee667098e8984f1ac0c5c7f8e
sogunsanya <109689290+sogunsanya@users.noreply.github.com>
2025-12-19 12:11:51 +0000

    basic setup,shadcnUi, themeProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import ThemeToggle from "@/components/shared/theme-toggle";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <h1 className="font-bold text-red-800">
-      Welcome to GoShop MultiVendor_Ecommerce
-    </h1>
+    <div>
+      <div className="w-100 flex justify-end">
+        {/* Theme Toggle Button */}
+        <ThemeToggle />
+      </div>
+
+      <h1 className="font-bold text-blue-800">
+        Welcome to GoShop MultiVendor_Ecommerce
+        <Button variant={"link"}>Click Me</Button>
+      </h1>
+    </div>
   );
 }
